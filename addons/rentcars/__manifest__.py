a8b9509cc2b2ec0fc17bc35d980836b7087efad3
tatitatou <tatiana.novion7@gmail.com>
2025-11-12 22:54:47 +0100

    tuto cours2 chap1-2-3

---
+++ addons/rentcars/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Location de véhicules",
+
+    'summary': "Outils de gestion de véhicules",
+
+    'description': """
+Outils de gestion de véhicules
+    """,
+
+    'author': "Tatiana Novion",
+    'website': "https://www.rentcarsTN.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Inventory/cars',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    'application': True,
+    'license': "AGPL-3",
+    'images': [
+       'static/description/icon.png',
+    ],
+
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
+
