c9c7abeb2f345324882a23b617396083e2ac9fa9
Tatiana Novion <tatiana.novion7@gmail.com>
2025-12-03 09:44:30 +0100

    cours 3 - chapitre 6 - champs calculés

--- addons/rentcars/models/rentcars_vehicle.py
+++ addons/rentcars/models/rentcars_vehicle.py
@@ -1,8 +1,10 @@
 #-*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 import re
 from odoo.exceptions import ValidationError
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 class Vehicle(models.Model):
     _name = 'rentcars.vehicle'
@@ -18,6 +20,8 @@ class Vehicle(models.Model):
     garage_id = fields.Many2one("rentcars.garage",string="garage")
     option_ids = fields.Many2many("rentcars.option",string="Option of vehicle")
     
+    age_vehicle = fields.Integer(compute="_age_vehicle", string="Age of vehicle (years)")
+    
     def _check_immatriculation(self):
         self.ensure_one()
         pattern = re.compile("\w{2}[0-9]{3}\w{2}$")
@@ -30,3 +34,10 @@ class Vehicle(models.Model):
             if vehicle.immatriculation and not vehicle._check_immatriculation():
                 raise ValidationError("%s Numberplate is invalid" % (vehicle.immatriculation))
         return True
+    
+    @api.depends('date_purchased')
+    def _age_vehicle(self):
+        for vehicle in self:
+            TODAY = date.today()
+            vehicle.age_vehicle=relativedelta(TODAY,vehicle.date_purchased).years
+            
